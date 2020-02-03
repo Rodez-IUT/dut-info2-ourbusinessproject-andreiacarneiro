3b644d07fa006b52bcebaf8b3c357d12902834f6
Andreia Carneiro <andreia.carneiro@iut-rodez.fr>
2020-02-03 14:02:26 +0100

    fix 1.3.1 persistance Project et Enterprise

--- src/main/java/ourbusinessproject/Project.java
+++ src/main/java/ourbusinessproject/Project.java
@@ -1,11 +1,21 @@
 package ourbusinessproject;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.NotEmpty;
 
+@Entity
 public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
 
     @NotEmpty
     private String title;
+
     private String description;
 
     public void setTitle(String title) {
