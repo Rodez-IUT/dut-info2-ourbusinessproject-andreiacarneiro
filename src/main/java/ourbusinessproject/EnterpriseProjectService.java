849fe03f436252713448e061b49d983409d60a46
Andreia Carneiro <andreia.carneiro@outlook.com>
2020-03-25 10:51:29 +0100

    fix #2.2.7 initialisation application OK

--- src/main/java/ourbusinessproject/EnterpriseProjectService.java
+++ src/main/java/ourbusinessproject/EnterpriseProjectService.java
@@ -6,10 +6,7 @@ import org.springframework.stereotype.Service;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
-import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
-import java.util.ListIterator;
 
 @Service
 public class EnterpriseProjectService {
