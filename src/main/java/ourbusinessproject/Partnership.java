f3f50a2cd216a30d9ba8f0b798e87f449be30921
Andreia Carneiro <andreia.carneiro@outlook.com>
2020-03-31 15:07:15 +0200

    fix #3.1 classe Partnership

---
+++ src/main/java/ourbusinessproject/Partnership.java
@@ -0,0 +1,40 @@
+package ourbusinessproject;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+public class Partnership {
+
+    private Long id;
+
+    @NotNull
+    private Date creationDate;
+
+    @NotNull
+    private Enterprise enterprise;
+
+    @NotNull
+    private Project project;
+
+    public Partnership(long id) {
+        this.id = id;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public void setEnterprise(Enterprise enterprise) {
+        this.enterprise = enterprise;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+
+
+
+
+
+}
