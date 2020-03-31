7f41f4ed92ca9e0c1d1300e37b98df7356cccee7
Andreia Carneiro <andreia.carneiro@outlook.com>
2020-03-31 11:40:58 +0200

    fix #2.5.2 generated SQL

--- src/main/java/ourbusinessproject/ProjectController.java
+++ src/main/java/ourbusinessproject/ProjectController.java
@@ -22,5 +22,17 @@ public class ProjectController {
     @RequestMapping(value = "/projectsWithEnterprises", method = RequestMethod.GET)
     public List<Project> findAllProjectsWithEnterprises() {
         return enterpriseProjectService.findAllProjects();
+
+        // Réponse 2.5.2:
+        // Pour l'affichage de tous les projets 3 requêtes sont générées :
+        //  - une pour obtenir tous les projets de la base de données
+        //    (basée sur la requête de findAllProjects)
+        //  - une requête pour obtenir les informations de la première entreprise
+        //  - une dernière pour obtenir les informations de la deuxième entreprise
+        // Ces deux dernières requêtes sont rajoutées par Hibernate car la requête
+        // de findAllProjects renvoie une liste de Projects et puisque ces objets
+        // Project contiennent un attribut Enterprise il faut aller chercher les
+        // informations de l'entreprise en question. Ces requêtes ne sont pas "inutiles"
+        // mais elles ne sont pas optimisées.
     }
 }
